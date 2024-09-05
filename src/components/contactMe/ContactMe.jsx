6b9302e9e0933fdb7f9871afa3690d463aae59ce
Vijay Bansal <134121986+VijayBansal2004@users.noreply.github.com>
2024-09-05 17:10:18 +0000

    worked on contact me section

--- src/components/contactMe/ContactMe.jsx
+++ src/components/contactMe/ContactMe.jsx
@@ -3,14 +3,20 @@ import styles from "./ContactMe.module.css"
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Heading from '../utilityComponents/sectionHeading/Heading';
+import SocialMediaLinks from '../utilityComponents/socialMedia/SocialMediaLinks';
 
 const ContactMe = () => {
     return (
         <section className={`${styles.contactMeContainer} text-white containerBlockPadding`}>
-            <div className="container">
-                <div className={`${styles.formContainer}`}>
+            <div className="container d-flex flex-column flex-lg-row justify-content-between align-items-center">
+                <div className={`${styles.socialMedia} w-25 text-center`}>
+                    <Heading heading="Social Media" />
+                    <div className="links d-flex gap-3 justify-content-center">
+                        <SocialMediaLinks />
+                    </div>
+                </div>
+                <div className={`${styles.formContainer} w-100`}>
                     <Heading heading="Contact Me" />
-
                     <Form className='d-grid gap-4'>
                         <div className="fadeUpAnimation">
                             <Form.Label htmlFor="inputPassword5">Enter Name</Form.Label>
