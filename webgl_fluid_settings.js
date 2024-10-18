e7eeebb93d9e94d56ac4c54de6b01e26c1c7966d
Vijay Bansal <134121986+VijayBansal2004@users.noreply.github.com>
2024-10-18 05:15:19 +0000

    added cursor-animation

---
+++ webgl_fluid_settings.js
@@ -0,0 +1,14 @@
+/* <![CDATA[ */
+var webgl_fluid_settings = {
+    color_type: "simple",
+    // color: "#0472B6",
+    color: "#FF9D3D",
+    resolution: "256",
+    // velocity: "2.9",
+    velocity: "2",
+    // density: "1.73",
+    density: "2",
+    pressure: "0.18",
+    splat_radius: "0.67",
+    backspace_animation: "yes",
+  };
